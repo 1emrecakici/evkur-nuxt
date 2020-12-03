9e189c45d294609013331476adfc86404a4100e9
1emrecakici <1emrecakici@gmail.com>
2020-12-03 15:49:33 +0300

    12

---
+++ pages/sepetim.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="shoppingcart-section">
+
+<div class="container">
+    <div class="checkout-title">
+        Alışveriş Sepeti
+        <a href="javascript:void(0)" class="button red mobile cart-form-submit-button">
+            <span>
+                Sonraki Adım
+            </span>
+        </a>
+            </div>
+    <div class="cart">
+            <div class="no-products">
+                Sepetinizde hiç ürün bulunmamaktadır.
+                <div>
+                    <a href="/ana-sayfa" class="button red">
+                        <span>Alışverişe Devam</span>
+                    </a>
+                </div>
+            </div>
+    </div>
+</div>
+</section>
+</template>
